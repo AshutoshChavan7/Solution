8b614f13ce6bc0534bffe92be8426cdf7aa94610
dac <dac@dypm.org>
2022-12-28 17:22:38 +0530

    mod

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:7
 WORKDIR /app
-COPY package.jason/app
+COPY package.jason /app
 RUN npm install
 COPY . /app
 CMD node app.js
 
-expose 3000
+EXPOSE 3000
--- frontend/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import './Header.css';
-const Header=()=>{
-  return(
-    <div id="mydiv1">
-         <h1>Employee management system</h1>
-    </div>
-  )
-}
-export default Header;
